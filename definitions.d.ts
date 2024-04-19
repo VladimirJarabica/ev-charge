38b7abb1c449b64e3f4063d1276f9222c36205e9
Vladimír Jarabica <vladojarabica@gmail.com>
2024-04-19 10:19:32 +0200

    Init

---
+++ definitions.d.ts
@@ -0,0 +1,7 @@
+declare module "exact-math" {
+  export function add(a: number, b: number): number;
+  export function sub(a: number, b: number): number;
+  export function mul(a: number, b: number): number;
+  export function div(a: number, b: number): number;
+  export function round(value: number, places: number): number;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,224 @@
-import Image from "next/image";
+"use client";
+import { useMemo, useState } from "react";
+import { Slider } from "../components/ui/slider";
+import { div, mul, round, sub } from "exact-math";
+import { Separator } from "../components/ui/separator";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../components/ui/table";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { Button } from "../components/ui/button";
 
 export default function Home() {
+  // const [state, setState] = useState<{
+  //   evConsumption: number;
+  //   kwPrice: number;
+  //   fuelConsumption: number;
+  //   fuelPrice: number;
+  // }>({
+  //   evConsumption: 20,
+  //   kwPrice: 0.39,
+  //   fuelConsumption: 7,
+  //   fuelPrice: 1.5,
+  // });
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const updateSearchParams = (key: string, value: string | null) => {
+    const params = new URLSearchParams(searchParams.toString());
+
+    if (value === null) {
+      console.log("delete", key);
+      params.delete(key);
+    } else {
+      params.set(key, value);
+    }
+
+    router.replace(`/?${params.toString()}`);
+  };
+
+  const evConsumption = Number(searchParams.get("evConsumption") ?? "20");
+  const kwPrice = Number(searchParams.get("kwPrice") ?? "0.49");
+  const chargingHome = searchParams.get("home") === "true";
+  const homeKwPrice = Number(searchParams.get("homeKwPrice") ?? "0.29");
+  const homeProportion = Number(searchParams.get("homeProportion") ?? "50");
+  const fuelConsumption = Number(searchParams.get("fuelConsumption") ?? "7");
+  const fuelPrice = Number(searchParams.get("fuelPrice") ?? "1.5");
+
+  const evCostPer100km = useMemo(() => {
+    const cost = round(mul(evConsumption, kwPrice), -2);
+    const homeCost = round(mul(evConsumption, homeKwPrice), -2);
+    const homeProportionToUse = chargingHome ? homeProportion : 0;
+    const totalCost = round(
+      div(
+        mul(cost, 100 - homeProportionToUse) +
+          mul(homeCost, homeProportionToUse),
+        100
+      ),
+      -2
+    );
+    return totalCost;
+  }, [chargingHome, evConsumption, homeKwPrice, homeProportion, kwPrice]);
+  const fossilCostPer100km = round(mul(fuelConsumption, fuelPrice), -2);
+
+  const differencePercentage = round(
+    div(mul(sub(evCostPer100km, fossilCostPer100km), 100), fossilCostPer100km),
+    -2
+  );
+
+  const differencePrice = round(sub(evCostPer100km, fossilCostPer100km), -2);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="flex min-h-screen flex-col items-center p-6 md:p-24">
+      <div className="text-xl font-semibold">EV car</div>
+      <div className="text-lg font-semibold mt-6 mb-3">
+        EV consumption: {evConsumption}kw/100km
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <Slider
+        defaultValue={[evConsumption]}
+        min={0.1}
+        max={30}
+        step={0.1}
+        onValueChange={([value]) =>
+          updateSearchParams("evConsumption", value.toString())
+        }
+      />
+      <div className="text-lg font-semibold mt-6 mb-3">
+        Price: {kwPrice}€/kw
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <Slider
+        defaultValue={[kwPrice]}
+        min={0.01}
+        max={1.5}
+        step={0.01}
+        onValueChange={([value]) =>
+          updateSearchParams("kwPrice", value.toString())
+        }
+      />
+      <Button
+        variant="secondary"
+        size="sm"
+        className="mt-6"
+        onClick={() => {
+          updateSearchParams("home", chargingHome ? null : "true");
+        }}
+      >
+        {chargingHome ? "Not charging home" : "Charging home"}
+      </Button>
+      {chargingHome && (
+        <>
+          <div className="text-lg font-semibold mt-6 mb-3">
+            Home charging price: {homeKwPrice}€/kw
+          </div>
+          <Slider
+            defaultValue={[homeKwPrice]}
+            min={0.01}
+            max={1.5}
+            step={0.01}
+            onValueChange={([value]) =>
+              updateSearchParams("homeKwPrice", value.toString())
+            }
+          />
+          <div className="text-lg font-semibold mt-6 mb-3">
+            Home charging proportion: {homeProportion}%
+          </div>
+          <Slider
+            defaultValue={[homeProportion]}
+            min={0}
+            max={100}
+            step={1}
+            onValueChange={([value]) =>
+              updateSearchParams("homeProportion", value.toString())
+            }
+          />
+        </>
+      )}
+      <Separator className="mt-10" />
+      <div className="text-xl font-semibold mt-6">Fossil car</div>
+      <div className="text-lg font-semibold mt-6 mb-3">
+        Fuel consumption: {fuelConsumption} litres/100km
       </div>
-    </main>
+      <Slider
+        defaultValue={[fuelConsumption]}
+        min={2}
+        max={30}
+        step={0.1}
+        onValueChange={([value]) =>
+          updateSearchParams("fuelConsumption", value.toString())
+        }
+      />
+      <div className="text-lg font-semibold mt-6 mb-3">
+        Fuel price: {fuelPrice}€/litre
+      </div>
+      <Slider
+        defaultValue={[fuelPrice]}
+        min={0.5}
+        max={3}
+        step={0.01}
+        onValueChange={([value]) =>
+          updateSearchParams("fuelPrice", value.toString())
+        }
+      />
+      <Table className="mt-10 text-lg">
+        <TableHeader className="font-semibold">
+          <TableRow>
+            <TableHead>Compare</TableHead>
+            <TableHead>EV</TableHead>
+            <TableHead>Fossil</TableHead>
+            <TableHead>Difference</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-semibold">Cost per 100km</TableCell>
+            <TableCell>{evCostPer100km}€</TableCell>
+            <TableCell>{fossilCostPer100km}€</TableCell>
+            <TableCell>
+              {differencePrice}€ ({differencePercentage}%)
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-semibold">Cost per 1km</TableCell>
+            <TableCell>{round(div(evCostPer100km, 100), -4)}€</TableCell>
+            <TableCell>{round(div(fossilCostPer100km, 100), -4)}€</TableCell>
+            <TableCell>
+              {round(div(differencePrice, 100), -4)}€ (
+              {round(div(differencePercentage, 100), -4)}%)
+            </TableCell>
+          </TableRow>
+        </TableBody>
+        {/* <TableHeader className="font-semibold">
+          <TableRow>
+            <TableHead>Difference</TableHead>
+            <TableHead>in %</TableHead>
+            <TableHead>in €</TableHead>
+           
+          </TableRow>
+        </TableHeader> */}
+        {/* <TableBody>
+          <TableCell></TableCell>
+          <TableCell>
+            {round(
+              div(
+                mul(sub(evCostPer100km, fossilCostPer100km), 100),
+                fossilCostPer100km
+              ),
+              -2
+            )}
+            %
+          </TableCell>
+          <TableCell>
+            {round(sub(evCostPer100km, fossilCostPer100km), -2)}€
+          </TableCell>
+        </TableBody> */}
+      </Table>
+    </div>
   );
 }
